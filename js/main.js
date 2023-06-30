f5106e275430afe680ca673aa379068ae10a8227
Kerry Cao <kcyq98@gmail.com>
2023-06-30 04:21:39 -0600

    antialias enabled

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.y = floor;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
